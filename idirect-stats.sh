55fbc9f38cf1161569fc511cf1921670f1bcea62
surinder432 <surinder.kumar.432@gmail.com>
2017-09-11 16:19:23 +0530

    Update idirect-stats.sh

--- idirect-stats.sh
+++ idirect-stats.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
-
+[surikuma@adc00oti ~/equity.d]$ cat idirect-stats.sh
 #!/bin/sh
 
 if test $# -lt 3
 then
-   echo "usage: $0 <file> <company | year> <all | tcs |  2017>"
+   echo "usage: $0 <file> <company | year | year_company> <all | tcs |  2017>"
    exit 1
 fi
 
@@ -46,7 +45,6 @@ then
 # Stock Symbol,Company Name,ISIN Code,Action,Quantity,Transaction Price,Brokerage,Transaction Charges,StampDuty,Segment,STT Paid/Not Paid,Remarks,Transaction Date,Exchange,
 # 3MIND,3M INDIA LIMITED,INE470A01017,Buy,1,13647.95,120.86,0.40,0.00,Rolling,STT Paid,icicidirect,11-Jul-2017,BSE,
 
-
 cat $FILE | grep -i $SPEC | awk -F',' '{ if (prev_company != $1) { prev_company=$1; prev_year=0; prev_month=0; printf("%s - %s\n",$1, $2);} tdate=$13; year=substr(tdate,8,4); month=substr(tdate,4,3); day=substr(tdate,1,2); if (prev_year != year) { prev_year=year; printf("Year %s\n",year);}  if (prev_month != month) { prev_month=month; printf("\t%s\n",month); } printf("\t\t%3s share@value %5s total %5s %4s\n", int($5), int ($6), int($5*$6), $4);   }' 
 fi
 
@@ -60,13 +58,26 @@ do
        # echo year $year
        # echo month $month
 
-       cat $FILE | grep $year | grep $SPEC | grep $month | awk -F','  ' BEGIN {tbuy = 0; tsale =0;} {tdate=$13; year=substr(tdate,8,4); month=substr(tdate,4,3); buysale=$4; if (buysale == "Buy") { tbuy += $5*$6; } else { tsale += $5*$6;} } END { if (tbuy != 0 || tsale != 0) {printf("%s %s #txn %3s sale %6s buy %6s net %6s\n", year, month, NR, int(tsale), int(tbuy), int(tbuy-tsale)); } }' 
+       cat $FILE | grep $year | grep $SPEC | grep $month | awk -F','  ' BEGIN {tbuy = 0; tsale =0; company=""; } {tdate=$13; year=substr(tdate,8,4); month=substr(tdate,4,3); buysale=$4; company=company" "$1; if (buysale == "Buy") { tbuy += $5*$6; } else { tsale += $5*$6;} } END { if (tbuy != 0 || tsale != 0) {printf("%s %s txn# %3s sale %6s buy %6s net %6s\n", year, month, NR, int(tsale), int(tbuy), int(tbuy-tsale)); } }' 
+   done
+done
+fi
+
+if [ "$ACTION" = "year_company" ]
+then
+
+for year in 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017
+do
+   for month in Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+   do
+       # echo year $year
+       # echo month $month
+
+       cat $FILE | grep $year | grep $SPEC | grep $month | awk -F','  ' BEGIN {tbuy = 0; tsale =0; company=""; } {tdate=$13; year=substr(tdate,8,4); month=substr(tdate,4,3); buysale=$4; company=company" "$1; if (buysale == "Buy") { tbuy += $5*$6; } else { tsale += $5*$6;} } END { if (tbuy != 0 || tsale != 0) {printf("%s %s txn# %3s sale %6s buy %6s net %6s companies %s\n", year, month, NR, int(tsale), int(tbuy), int(tbuy-tsale), company); } }' 
    done
 done
 fi
 
 # end of the file
 ################################
-
-
 
