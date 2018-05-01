736043dd5c52803e04db26a3fa4ff83c632496c3
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-05-01 09:34:40 -0700

    modified:   dividend-reports.sh 	renamed:    reports/dividends/fy1617-sort_amount.csv -> reports/dividends/fy1617/fy1617-sort_amount.csv 	renamed:    reports/dividends/fy1617-sort_frequency.csv -> reports/dividends/fy1617/fy1617-sort_frequency.csv 	renamed:    reports/dividends/fy1617-sort_name.csv -> reports/dividends/fy1617/fy1617-sort_name.csv 	renamed:    reports/dividends/fy1718-sort_amount.csv -> reports/dividends/fy1718/fy1718-sort_amount.csv 	renamed:    reports/dividends/fy1718-sort_frequency.csv -> reports/dividends/fy1718/fy1718-sort_frequency.csv 	renamed:    reports/dividends/fy1718-sort_name.csv -> reports/dividends/fy1718/fy1718-sort_name.csv

--- dividend-reports.sh
+++ dividend-reports.sh
@@ -4,7 +4,7 @@ for FY in fy1617 fy1718
 do
   for SORT_TYPE in sort_name sort_amount sort_frequency 
   do
-     icici-dividend-stats.py ./sample-data/bank-txn-hist/${FY}/OpTransactionHistory-${FY}.csv csv ${SORT_TYPE} > reports/dividends/${FY}-${SORT_TYPE}.csv
+     icici-dividend-stats.py ./sample-data/bank-txn-hist/${FY}/OpTransactionHistory-${FY}.csv csv ${SORT_TYPE} > reports/dividends/${FY}/${FY}-${SORT_TYPE}.csv
   done
 done
 
