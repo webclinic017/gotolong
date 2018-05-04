9bc42914d43e3dc3565d12fd8b9274be92de0817
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-05-04 09:54:15 -0700

    new file:   reports/decisions/all/dividend_plus_holdings_zero.txt 	new file:   reports/decisions/all/holdings_plus_dividend_zero.txt 	new file:   src/future_decisions.sh

---
+++ src/future_decisions.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+holdings_plus_dividend_zero.sh > $PROJ_REPORTS_LOC/decisions/all/holdings_plus_dividend_zero.txt
+
+dividend_plus_holdings_zero.sh > $PROJ_REPORTS_LOC/decisions/all/dividend_plus_holdings_zero.txt
