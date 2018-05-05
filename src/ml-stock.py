dbdf30198ca96b6dc7b7101485671a03a69145f2
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-05-05 10:34:02 -0700

    new file:   ml-stock.py

---
+++ src/ml-stock.py
@@ -0,0 +1,48 @@
+#!/bin/python
+
+import quandl
+import pandas as pd
+import numpy as np
+import datetime
+
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, cross_validation, svm
+
+df = quandl.get("WIKI/AMZN")
+
+print(df.tail())
+
+df = df[['Adj. Close']]
+
+forecast_out = int(30) # predicting 30 days into future
+
+#  label column with data shifted 30 units up
+df['Prediction'] = df[['Adj. Close']].shift(-forecast_out) 
+
+print(df.tail())
+
+X = np.array(df.drop(['Prediction'], 1))
+X = preprocessing.scale(X)
+
+X_forecast = X[-forecast_out:] # set X_forecast equal to last 30
+X = X[:-forecast_out] # remove last 30 from X
+
+y = np.array(df['Prediction'])
+y = y[:-forecast_out]
+
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+
+
+# Training
+clf = LinearRegression()
+clf.fit(X_train,y_train)
+
+# Testing
+confidence = clf.score(X_test, y_test)
+print("confidence: ", confidence)
+
+
+forecast_prediction = clf.predict(X_forecast)
+print(forecast_prediction)
+
