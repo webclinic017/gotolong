7f922fc2ceda89955e1d1b1f6392d4206b8e6472
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-05-10 08:08:24 -0700

    renamed:    data/sample-data/bse-hist/fy1617/bse-corporate_actions-dividend-fy1617.csv -> data/sample-data/bse-hist/fy1617/bse-corp-action-dividend-fy1617.csv 	new file:   reports/dividends/all/bse-all-sort_frequency.csv 	new file:   reports/dividends/fy1516/bse-fy1516-sort_frequency.csv 	new file:   reports/dividends/fy1617/bse-fy1617-sort_frequency.csv 	modified:   src/dividend-bse.sh

--- src/dividend-bse.sh
+++ src/dividend-bse.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-dividend-bse.py out_plain sort_frequency summary_yes 1 
-
-
+OUT_TYPE=out_csv
 DEBUG_LEVEL=0
 SUMMARY_TYPE=summary_yes
 
-for FY in fy1718
+for FY in fy1516 fy1617 fy1718 all
 do
   for SORT_TYPE in sort_frequency 
   do
-     	./dividend-bse.py out_csv ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${PROJ_DATA_LOC}/sample-data/bse-hist/${FY}/bse-*.csv > ${PROJ_REPORTS_LOC}/dividends/${FY}/bse-${FY}-${SORT_TYPE}.csv
+	if [[ "${FY}" == "all" ]] ; then
+     	./dividend-bse.py ${OUT_TYPE} ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${PROJ_DATA_LOC}/sample-data/bse-hist/*/bse-*.csv > ${PROJ_REPORTS_LOC}/dividends/${FY}/bse-${FY}-${SORT_TYPE}.csv
+	else
+     	./dividend-bse.py ${OUT_TYPE} ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${PROJ_DATA_LOC}/sample-data/bse-hist/${FY}/bse-*.csv > ${PROJ_REPORTS_LOC}/dividends/${FY}/bse-${FY}-${SORT_TYPE}.csv
+	fi
   done
 done
 
