d40cefd4b9e0e93a4b9a9b792e1caef573d08f15
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-05-29 06:55:29 -0700

    modified:   reports/targets/all/all-sector_industry_company.txt 	modified:   src/target-stats.py

--- src/target-stats.py
+++ src/target-stats.py
@@ -102,8 +102,10 @@ def myprint(d, stack_depth):
 			pv_total += int(float(planned_value))
 			cv_total += int(float(current_value))
 			tbd_total += int(float(tbd_value))
-			if company_count == 5:
+			if company_count == 4:
 				print('')
+			if (company_count == 1) or (company_count == 4):
+			 	sys.stdout.write('  ')	
 			sys.stdout.write(k + '(' + v +')' + ' | ')
 	print('')		
 	return pv_total, cv_total, tbd_total 
