ad381093e3376033e4ede04a9ad65e5124778615
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-08-15 08:03:40 -0700

    renamed:    data/sample-data/bank-txn-hist/fy1617/OpTransactionHistory-fy1617.csv -> profile/default/data/bank-txn-data/fy1617/OpTransactionHistory-fy1617.csv 	renamed:    data/sample-data/bank-txn-hist/fy1617/readme.txt -> profile/default/data/bank-txn-data/fy1617/readme.txt 	renamed:    data/sample-data/bank-txn-hist/fy1718/OpTransactionHistory-fy1718.csv -> profile/default/data/bank-txn-data/fy1718/OpTransactionHistory-fy1718.csv 	renamed:    data/sample-data/bank-txn-hist/fy1718/readme.txt -> profile/default/data/bank-txn-data/fy1718/readme.txt 	renamed:    data/sample-data/bank-txn-hist/readme.txt -> profile/default/data/bank-txn-data/readme.txt 	new file:   profile/default/reports/dividend-reports/all/all-name_only.csv 	new file:   profile/default/reports/dividend-reports/all/all-sort_amount.csv 	new file:   profile/default/reports/dividend-reports/all/all-sort_frequency.csv 	new file:   profile/default/reports/dividend-reports/all/all-sort_name.csv 	new file:   profile/default/reports/dividend-reports/fy1617/fy1617-name_only.csv 	new file:   profile/default/reports/dividend-reports/fy1617/fy1617-sort_amount.csv 	new file:   profile/default/reports/dividend-reports/fy1617/fy1617-sort_frequency.csv 	new file:   profile/default/reports/dividend-reports/fy1617/fy1617-sort_name.csv 	new file:   profile/default/reports/dividend-reports/fy1718/fy1718-name_only.csv 	new file:   profile/default/reports/dividend-reports/fy1718/fy1718-sort_amount.csv 	new file:   profile/default/reports/dividend-reports/fy1718/fy1718-sort_frequency.csv 	new file:   profile/default/reports/dividend-reports/fy1718/fy1718-sort_name.csv 	renamed:    src/all-reports.sh -> src/decisions/all-reports.sh 	renamed:    src/decisions-reports.sh -> src/decisions/decisions-reports.sh 	renamed:    src/dividend/dividend_plus_holdings_zero.sh -> src/decisions/dividend_plus_holdings_zero.sh 	renamed:    src/holdings_plus_dividend_zero.sh -> src/decisions/holdings_plus_dividend_zero.sh 	modified:   src/dividend/dividend-reports.sh

--- src/dividend/dividend-reports.sh
+++ src/dividend/dividend-reports.sh
@@ -3,14 +3,16 @@
 DEBUG_LEVEL=0
 SUMMARY_TYPE=summary_yes
 
+IN_DIR=$PROJ_PROFILE_DATA_LOC/bank-txn-data/
+OUT_DIR=$PROJ_PROFILE_REPORTS_LOC/dividend-reports/
 for FY in fy1617 fy1718 all
 do
   for SORT_TYPE in name_only sort_name sort_amount sort_frequency 
   do
       if [[ "${FY}" == "all" ]]; then
-     	dividend-stats.py out_csv ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${PROJ_DATA_LOC}/sample-data/bank-txn-hist/*/OpTransactionHistory-*.csv > ${PROJ_REPORTS_LOC}/dividends/${FY}/${FY}-${SORT_TYPE}.csv
+     	dividend-stats.py out_csv ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${IN_DIR}/*/OpTransactionHistory-*.csv > ${OUT_DIR}/${FY}/${FY}-${SORT_TYPE}.csv
       else
-     	dividend-stats.py out_csv ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${PROJ_DATA_LOC}/sample-data/bank-txn-hist/${FY}/OpTransactionHistory-${FY}.csv > ${PROJ_REPORTS_LOC}/dividends/${FY}/${FY}-${SORT_TYPE}.csv
+     	dividend-stats.py out_csv ${SORT_TYPE} ${SUMMARY_TYPE} ${DEBUG_LEVEL} ${IN_DIR}/${FY}/OpTransactionHistory-${FY}.csv > ${OUT_DIR}/${FY}/${FY}-${SORT_TYPE}.csv
       fi
   done
 done
