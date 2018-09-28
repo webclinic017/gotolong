b1ea19c67c5d737c9e245bffc31990280608aa70
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-09-28 00:43:06 -0700

    deleted:    profile/default/data/demat-data/icicidirect/.demat-summary-data.csv.swp 	new file:   profile/default/reports/sfund-demat-reports/sfund-demat-dumb-diff.csv 	new file:   profile/default/reports/sfund-demat-reports/sfund-demat-dumb-join.csv 	modified:   src/sfund-demat/sfund_demat_anomaly.sh

--- src/sfund-demat/sfund_demat_anomaly.sh
+++ src/sfund-demat/sfund_demat_anomaly.sh
@@ -4,6 +4,10 @@ REPORTS_SFUND_FILE=$PROJ_PROFILE_REPORTS_LOC/sfund-demat-reports/sfund-names.csv
 
 REPORTS_DEMAT_FILE=$PROJ_PROFILE_REPORTS_LOC/sfund-demat-reports/demat-summary-names.csv
 
-REPORTS_SFUND_DEMAT_FILE=$PROJ_PROFILE_REPORTS_LOC/sfund-demat-reports/sfund-demat-diff.csv
 
-sdiff ${REPORTS_SFUND_FILE} ${REPORTS_DEMAT_FILE}  > ${REPORTS_SFUND_DEMAT_FILE}
+REPORTS_SFUND_DEMAT_DIFF_FILE=$PROJ_PROFILE_REPORTS_LOC/sfund-demat-reports/sfund-demat-dumb-diff.csv
+
+REPORTS_SFUND_DEMAT_JOIN_FILE=$PROJ_PROFILE_REPORTS_LOC/sfund-demat-reports/sfund-demat-dumb-join.csv
+
+sdiff -s -W -B ${REPORTS_SFUND_FILE} ${REPORTS_DEMAT_FILE}  > ${REPORTS_SFUND_DEMAT_DIFF_FILE}
+paste -d, ${REPORTS_SFUND_FILE} ${REPORTS_DEMAT_FILE}  > ${REPORTS_SFUND_DEMAT_JOIN_FILE}
