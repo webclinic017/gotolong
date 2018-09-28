1a5dc1131380aeed956aa31dee64281e4972ec6a
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-09-28 02:59:20 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   profile/default/data/sfund-data/sfund-data.csv
    	modified:   profile/default/reports/sfund-demat-reports/sfund-demat-dumb-diff.csv
    	modified:   profile/default/reports/sfund-demat-reports/sfund-demat-dumb-join.csv
    	modified:   profile/default/reports/sfund-demat-reports/sfund-names.csv
    	modified:   src/sfund-demat/sfund_company_names.sh

--- src/sfund-demat/sfund_company_names.sh
+++ src/sfund-demat/sfund_company_names.sh
@@ -6,5 +6,5 @@ REPORTS_SFUND_FILE=$PROJ_PROFILE_REPORTS_LOC/sfund-demat-reports/sfund-names.csv
 awk -F "," '
 { if (start_printing) { for (i=4; i<=NF;i++) {print $i} } }
 /Category/ { if ($1 == "Category") {start_printing=1} } 
-' ${DATA_SFUND_FILE} | tr '[a-z]' '[A-Z]' | sort -u | grep -v -e '#'  | sed '/^$/d' | sed -e 's/ INDIA//g' -e 's/LIMITED//g' -e 's/LTD//g' -e 's/ OF / /g' -e 's/ & / /g' -e 's/The / /g' > ${REPORTS_SFUND_FILE}
+' ${DATA_SFUND_FILE} | tr '[a-z]' '[A-Z]' | sort -u | grep -v -e '#'  | sed '/^$/d' | sed -e 's/ INDIA//g' -e 's/LIMITED//g' -e 's/LTD//g' -e 's/ OF / /g' -e 's/ & / /g' -e 's/&//g' -e 's/The / /g' > ${REPORTS_SFUND_FILE}
 
