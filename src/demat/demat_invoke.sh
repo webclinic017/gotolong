afb3a8cd8dc312f2accbd02a0a24cccbfea645f6
Surinder Kumar <surinder.kumar.432@gmail.com>
2018-11-08 22:49:32 -0800

    new file:   profile/default/reports/demat-reports/stats_qtr_output.csv 	new file:   profile/default/reports/demat-reports/stats_year_output.csv 	deleted:    profile/default/reports/demat-txn-reports/year_company_all_all_qtr_output.csv 	deleted:    profile/default/reports/demat-txn-reports/year_company_all_all_year_output.csv 	deleted:    src/cutil/.cutil.py.swp 	new file:   src/demat/awkvars.out 	modified:   src/demat/demat_invoke.sh 	renamed:    src/demat/icici-direct-stats.sh -> src/demat/demat_stats/icici_direct_stats.sh 	new file:   src/target/sort.csv

--- src/demat/demat_invoke.sh
+++ src/demat/demat_invoke.sh
@@ -9,3 +9,8 @@ OUT_FILE_3=$PROJ_PROFILE_REPORTS_LOC/demat-reports/portfolio-phase-3.csv
 OUT_FILE_4=$PROJ_PROFILE_REPORTS_LOC/demat-reports/portfolio-phase-4.csv
 
 demat_invoke.py ${DEBUG_LEVEL} ${IN_FILE} ${OUT_FILE_1} ${OUT_FILE_2} ${OUT_FILE_3} ${OUT_FILE_4} 
+
+OUT_FILE_QTR=$PROJ_PROFILE_REPORTS_LOC/demat-reports/stats_qtr_output.csv
+OUT_FILE_YEAR=$PROJ_PROFILE_REPORTS_LOC/demat-reports/stats_year_output.csv
+./demat_stats/icici_direct_stats.sh ${IN_FILE} all all year > ${OUT_FILE_YEAR}
+./demat_stats/icici_direct_stats.sh ${IN_FILE} all all quarter > ${OUT_FILE_QTR}
