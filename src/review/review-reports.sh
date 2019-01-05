93243d1651b6dec55c2bea640760bfef062d1f40
Surinder Kumar <surinder.kumar.432@gmail.com>
2019-01-04 23:13:07 -0800

    new file:   resources/profile/default/reports/review-reports/review_plan_plus_dividend_none-.txt 	modified:   src/review/review-reports.sh

--- src/review/review-reports.sh
+++ src/review/review-reports.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-DIV_NAME=${PROJ_PROFILE_REPORTS_LOC}/dividend-reports/all/all-name_only.csv
 PLAN_NAME=${PROJ_PROFILE_REPORTS_LOC}/plan-reports/plan-reports-name-only.csv
 
 mkdir -p ${PROJ_PROFILE_REPORTS_LOC}/review-reports
 
-comm -13 ${DIV_NAME} ${PLAN_NAME} > ${PROJ_PROFILE_REPORTS_LOC}/review-reports/review_plan_plus_dividend_none.txt
+for DIV_YEAR in fy1516 fy1617 fy1718 fy1819 all
+do
+
+DIV_NAME=${PROJ_PROFILE_REPORTS_LOC}/dividend-reports/${DIV_YEAR}/${DIV_YEAR}-name_only.csv
+
+comm -13 ${DIV_NAME} ${PLAN_NAME} > ${PROJ_PROFILE_REPORTS_LOC}/review-reports/review_plan_plus_dividend_none-${DIVY_YEAR}.txt
+done
 
 # should be done later
 # comm -23 ${DIV_NAME} ${PLAN_NAME} > ${PROJ_PROFILE_REPORTS_LOC}/review-reports/review_plan_none_dividend_plus.txt
