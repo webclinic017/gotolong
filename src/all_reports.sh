73838c138efa027ee0bc826eab003e17a2a16a2b
surinder kumar <surinder.kumar.432@gmail.com>
2019-12-16 21:23:17 +0530

    bank-statement parser for sbi bank and hdfc bank

--- src/all_reports.sh
+++ src/all_reports.sh
@@ -1,7 +1,49 @@
 #!/bin/sh
 
-# screener target tbd
-for TARGET in amfi isin plan weight demat dividend screener uhealth anomaly
+# AMFI & ISIN
+for TARGET in amfi isin
+do
+  cd $PROJ_SOURCE_LOC/$TARGET
+  ./*.sh
+done
+
+
+for TARGET in demat-idirect demat-zerodha demat-txn-stat
+do
+  cd $PROJ_SOURCE_LOC/$TARGET
+  ./*.sh
+done
+
+
+for TARGET in bank-stmt-txn-parser dividend
+do
+  cd $PROJ_SOURCE_LOC/$TARGET
+  ./*.sh
+done
+
+
+for TARGET in screener trendlyne chealth gfin
+do
+  cd $PROJ_SOURCE_LOC/$TARGET
+  ./*.sh
+done
+
+
+for TARGET in anomanly
+do
+  cd $PROJ_SOURCE_LOC/$TARGET
+  ./*.sh
+done
+
+
+for TARGET in bse
+do
+  cd $PROJ_SOURCE_LOC/$TARGET
+  ./*.sh
+done
+
+
+for TARGET in plan weight
 do
   cd $PROJ_SOURCE_LOC/$TARGET
   ./*.sh
